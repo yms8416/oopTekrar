075b1e638d9cdc58df9e8481adfd0945ba77bd73
Can PERK <bayms8416@gmail.com>
2018-03-18 12:41:40 +0300

    stock management

---
+++ BilgeAdam.OOP.StockManagement/Helpers/TextHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace BilgeAdam.OOP.StockManagement.Helpers
+{
+    public class TextHelper
+    {
+        private static Random rnd = new Random();
+        public static string GenerateCode()
+        {
+            var code = rnd.Next(100000000, 999999999);
+            var result = Regex.Replace(code.ToString(), @"^(.{3})(.{4})(.{2})$", "$1-$2-$3");
+            return result;
+        }
+    }
+}
---
+++ BilgeAdam.OOP.StockManagement/Models/Base/ProductBase.cs
@@ -0,0 +1,21 @@
+﻿using BilgeAdam.OOP.StockManagement.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilgeAdam.OOP.StockManagement.Models
+{
+    public abstract class ProductBase
+    {
+        public ProductBase()
+        {
+            Barcode = TextHelper.GenerateCode();
+        }
+        public string Barcode { get; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int UnitsInStock { get; set; }
+    }
+}
